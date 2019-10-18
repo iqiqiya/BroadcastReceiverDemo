d0ae25ab03583c32e9a152a8e2fed556918e2e64
iqiqiya <iqiqiya@outlook.com>
2019-10-18 21:49:39 +0800

    自定义广播并动态注册

--- app/src/main/java/iqiqiya/lanlana/broadcastreceiverdemo/BroadcastReceiverTest.java
+++ app/src/main/java/iqiqiya/lanlana/broadcastreceiverdemo/BroadcastReceiverTest.java
@@ -3,7 +3,11 @@ package iqiqiya.lanlana.broadcastreceiverdemo;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.text.TextUtils;
 import android.util.Log;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * Author: iqiqiya
@@ -14,6 +18,15 @@ import android.util.Log;
  */
 public class BroadcastReceiverTest extends BroadcastReceiver {
 
+    TextView mTextView;
+    public BroadcastReceiverTest(){
+
+    }
+
+    public BroadcastReceiverTest(TextView textView){
+        mTextView = textView;
+    }
+
     private static final String TAG = "BroadcastReceiverTest";
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -22,6 +35,15 @@ public class BroadcastReceiverTest extends BroadcastReceiver {
             // 接收到的是什么广播
             String action = intent.getAction();
             Log.d(TAG, "onReceive: "+ action);
+
+            // 判断是否是自己发送的广播
+            if (TextUtils.equals(action, MainActivity.MY_ACTION)){
+                // 获取广播携带的内容
+                String content = intent.getStringExtra(MainActivity.broadcast_content);
+                if (mTextView != null){
+                    mTextView.setText("接收到的action是："+ action+"\n接收到的内容是：\n"+content);
+                }
+            }
         }
     }
 }
--- app/src/main/java/iqiqiya/lanlana/broadcastreceiverdemo/MainActivity.java
+++ app/src/main/java/iqiqiya/lanlana/broadcastreceiverdemo/MainActivity.java
@@ -5,33 +5,66 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String MY_ACTION = "iqiiqya.lanlana";
+    public static final String broadcast_content = "broadcast_content";
     private BroadcastReceiverTest broadcastReceiverTest;
+    private EditText minputEditText;
+    private Button msendBroadcastButton;
+    public TextView mresultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initView();
+
         // 新建一个广播接收器
-        broadcastReceiverTest = new BroadcastReceiverTest();
+        broadcastReceiverTest = new BroadcastReceiverTest(mresultTextView);
 
         // 要接收哪些广播
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(Intent.ACTION_PACKAGE_REMOVED);
-        intentFilter.addDataScheme("package");
+        //intentFilter.addDataScheme("package");
+        intentFilter.addAction(MY_ACTION);
 
-        intentFilter.addAction(Intent.ACTION_BATTERY_LOW);
+        //intentFilter.addAction(Intent.ACTION_BATTERY_LOW);
 
         // 注册广播接收器
         registerReceiver(broadcastReceiverTest,intentFilter);
+
+        msendBroadcastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 新建广播
+                Intent intent = new Intent(MY_ACTION);
+                // 放入广播需要携带的数据
+                intent.putExtra(broadcast_content,minputEditText.getText());
+                sendBroadcast(intent);
+            }
+        });
+    }
+
+    private void initView() {
+        minputEditText = findViewById(R.id.inputEditText);
+        msendBroadcastButton = findViewById(R.id.sendBroadcastButton);
+        mresultTextView = findViewById(R.id.resultTextView);
+
+
     }
 
     protected void onDestroy(){
         super.onDestroy();
-        // 取消广播接收器
+        // 取消注册广播接收器
         if (broadcastReceiverTest != null){
             unregisterReceiver(broadcastReceiverTest);
         }
